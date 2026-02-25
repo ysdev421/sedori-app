d3d79ea5d621b6534375c4886d90ef89d8fbee29
yutaroshiraishi <yutaro.shiraishi.0421@gmail.com>
2026-02-25 17:08:12 +0900

    fix:環境変数未設定時に白画面ではなくエラー表示

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const requiredEnvKeys = [
+  'VITE_FIREBASE_API_KEY',
+  'VITE_FIREBASE_AUTH_DOMAIN',
+  'VITE_FIREBASE_PROJECT_ID',
+  'VITE_FIREBASE_STORAGE_BUCKET',
+  'VITE_FIREBASE_MESSAGING_SENDER_ID',
+  'VITE_FIREBASE_APP_ID',
+] as const;
+
+const missingEnv = requiredEnvKeys.filter((key) => !import.meta.env[key]);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+if (missingEnv.length > 0) {
+  root.render(
+    <React.StrictMode>
+      <div style={{ fontFamily: 'sans-serif', padding: '24px', lineHeight: 1.6 }}>
+        <h1>Configuration Error</h1>
+        <p>Firebase environment variables are missing.</p>
+        <p>Please set the following variables in your deployment environment:</p>
+        <pre>{missingEnv.join('\n')}</pre>
+      </div>
+    </React.StrictMode>
+  );
+} else {
+  import('./App.tsx').then(({ default: App }) => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  });
+}
